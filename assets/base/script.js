129ec1c79252d3f17b84b6cc8ceafc6c7719fb38
diegogithub-coding <54487369+diegogithub-coding@users.noreply.github.com>
2023-07-24 23:17:44 -0600

    update

--- assets/base/script.js
+++ assets/base/script.js
@@ -8,26 +8,3 @@ var days = difdt.getDate() - (difdt.getMonth() != 0 ? 0 : 1);
           (days != 0 ? " y " + days + " Día" + (days == 1 ? "" : "s") : ""));*/
 
 age.text((difdt.toISOString().slice(0, 4) - 1970) + " Años");
-        // Function to generate and download the image
-        function generateImage() {
-            // Get the entire HTML content of the page
-            const htmlContent = document.documentElement;
-
-            // Use html2canvas to capture the content as an image
-            html2canvas(htmlContent).then(function(canvas) {
-                // Convert the canvas to an image data URL
-                const imageDataURL = canvas.toDataURL("image/png");
-
-                // Create a temporary link element to trigger the download
-                const downloadLink = document.createElement("a");
-                downloadLink.href = imageDataURL;
-                downloadLink.download = "your-website-image.png";
-
-                // Append the link to the document and click it programmatically to trigger the download
-                document.body.appendChild(downloadLink);
-                downloadLink.click();
-
-                // Clean up by removing the temporary link element
-                document.body.removeChild(downloadLink);
-            });
-        }
